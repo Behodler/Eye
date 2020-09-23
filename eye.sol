18200c2c8e99d6d6aac0088de00333958f7bfb74
Justin Goro <justingoro@protonmail.com>
2020-09-23 07:36:14 +0200

    permissioned burning

--- eye.sol
+++ eye.sol
@@ -76,12 +76,66 @@ abstract contract Context {
     }
 }
 
-contract Eye is Context, IERC20 {
+contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+
+contract Eye is Context, Ownable, IERC20 {
     uint256 public constant ONE = 1e18;
     using SafeMath for uint256;
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
     uint256 private _totalSupply = 10e6 * ONE;
+    bool private burnEnabled = false;
 
     constructor() {
         _balances[msg.sender] = _totalSupply;
@@ -211,7 +265,12 @@ contract Eye is Context, IERC20 {
         emit Approval(owner, spender, amount);
     }
 
+    function enableBurning(bool enabled) public onlyOwner {
+        burnEnabled = enabled;
+    }
+
     function burn (uint value) public {
+        require(burnEnabled,"burn feature not yet active.");
         _burn(_msgSender(),value);
     }
 
